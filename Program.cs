8b3cdcae152152977f9239a6a2b4e162f8dbc56c
nathanR <nrice@truecoders.io>
2020-07-24 19:49:58 -0500

    Finished API build

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System;
+using System.Runtime.InteropServices;
 
 namespace OpenWeatherMap
 {
@@ -6,7 +9,14 @@ namespace OpenWeatherMap
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var defaultKey = System.IO.File.ReadAllText("appsettings.json");
+            Console.Write("Please enter your Zipcode: ");
+            var zip = Console.ReadLine();
+            Console.Write("Please enter your Country Code: ");
+            var country = Console.ReadLine();
+
+            var weather = WeatherAPI.GetWeather(zip, country, defaultKey);
+            Console.WriteLine(weather);
         }
     }
 }
---
+++ WeatherAPI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+
+namespace OpenWeatherMap
+{
+    class WeatherAPI
+    {
+        public static string GetWeather(string zip, string country, string defaultKey)
+        {
+
+            var apiKey = JObject.Parse(defaultKey).GetValue("DefaultKey").ToString();
+            var appID = "&appid=";
+            var apiAddress = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + "," + country;
+            var apiCall = $"{apiAddress}{appID}{apiKey}";
+
+            var client = new HttpClient();
+            var response = client.GetStringAsync(apiCall).Result;
+
+            var currentWeather = JObject.Parse(response).GetValue("main").ToString();
+            return currentWeather;
+        }
+    }
+}
