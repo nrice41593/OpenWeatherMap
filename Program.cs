f5e9e7c1a3d7ccfef655db09f37f46e5c14f808c
nathanR <nrice@truecoders.io>
2020-07-24 19:20:15 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenWeatherMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
